d60e795c344538727787579ff4eb80bbd889828a
Sonam24751 <Sonampal9219@gmail.com>
2025-06-16 16:01:18 +0530

    Update resized.py

--- resized.py
+++ resized.py
@@ -4,18 +4,7 @@ import numpy as np
 import cv2 
 import io
 
-# Hide Streamlit Cloud elements
-hide_streamlit_cloud_elements = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    header {visibility: hidden;}
-    footer {visibility: hidden;}
-    .stDeployButton {display: none;}
-    a[title="View source"] {display: none !important;}
-    button[kind="icon"] {display: none !important;}
-    </style>
-"""
-st.markdown(hide_streamlit_cloud_elements, unsafe_allow_html=True)
+
 
 # Set page title and layout
 st.set_page_config(page_title="Photo Resizer", layout="centered")
